1fde0e944ae013444e2e8b096ba283a11af7bbd1
amalsyahmi-certifai <76939137+amalsyahmi-certifai@users.noreply.github.com>
2022-05-11 11:21:59 +0800

    Fix main app file

--- App.tsx
+++ App.tsx
@@ -71,8 +71,8 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            <Text>Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.</Text>
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
